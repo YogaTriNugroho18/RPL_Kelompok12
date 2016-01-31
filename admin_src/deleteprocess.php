e11427c0465f7cb68326acf45485a72b235036f3
Yoga Tri Nugroho <renascent_3n@email.unikom.ac.id>
2016-01-31 21:45:42 +0700

    Update deleteprocess.php

--- admin_src/deleteprocess.php
+++ admin_src/deleteprocess.php
@@ -5,6 +5,6 @@ $id = $_GET['id'];
 $query = "DELETE FROM employee WHERE employee_id = '$id'";
 $sql = mysql_query($query);
 if ($sql) {
-	echo '<script>alert("Data successfully deleted");window.location="../admin";</script>';
+	echo '<script>alert("Data successfully deleted");window.location="../admin.php";</script>';
 }
 ?>
