b86e50f2f368f6db781dffbbf4ba416076c7dc41
Yoga Tri Nugroho <renascent_3n@email.unikom.ac.id>
2016-01-31 22:12:21 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,36 +3,36 @@ session_start();
 if (isset($_SESSION['admin'])) {
 	unset($_SESSION['admin']);
 	session_destroy();
-	echo '<script>window.location="home";</script>';
+	echo '<script>window.location="home.php";</script>';
 }
 
 if (isset($_SESSION['waiter'])) {
 	unset($_SESSION['waiter']);
 	session_destroy();
-	echo '<script>window.location="home";</script>';
+	echo '<script>window.location="home.php";</script>';
 }
 
 if (isset($_SESSION['chef'])) {
 	unset($_SESSION['chef']);
 	session_destroy();
-	echo '<script>window.location="home";</script>';
+	echo '<script>window.location="home.php";</script>';
 }
 
 if (isset($_SESSION['pantry'])) {
 	unset($_SESSION['pantry']);
 	session_destroy();
-	echo '<script>window.location="home";</script>';
+	echo '<script>window.location="home.php";</script>';
 }
 
 if (isset($_SESSION['cs'])) {
 	unset($_SESSION['cs']);
 	session_destroy();
-	echo '<script>window.location="home";</script>';
+	echo '<script>window.location="home.php";</script>';
 }
 
 if (isset($_SESSION['cashier'])) {
 	unset($_SESSION['cashier']);
 	session_destroy();
-	echo '<script>window.location="home";</script>';
+	echo '<script>window.location="home.php";</script>';
 }
 ?>
