8d5492b550920299c6bcd383d28fa334a90446cc
Yoga Tri Nugroho <renascent_3n@email.unikom.ac.id>
2016-01-31 21:51:34 +0700

    Update paid.php

--- cashier_src/paid.php
+++ cashier_src/paid.php
@@ -5,7 +5,7 @@ $id = $_GET['id'];
 if (isset($_POST['paid'])) {
 	$query = mysql_query("UPDATE payments SET status = '1' WHERE payment_id = '$id'");
 	if ($query) {
-		echo '<script>alert("Order Success");window.location="../cashier";</script>';
+		echo '<script>alert("Order Success");window.location="../cashier.php";</script>';
 	} else {
 		echo mysql_errno();
 		die();
